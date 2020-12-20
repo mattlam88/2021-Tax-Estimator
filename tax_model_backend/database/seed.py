372c10b46c5586425b2a7a3393ade423748a9d56
mattlam88 <63694045+mattlam88@users.noreply.github.com>
2020-12-20 02:36:38 -0500

    state tax rates included in database table

--- tax_model_backend/database/seed.py
+++ tax_model_backend/database/seed.py
@@ -1,4 +1,5 @@
 import sqlite3
+import csv
 
 conn = sqlite3.connect("taxModel.db")
 c = conn.cursor()
@@ -7,13 +8,18 @@ c.execute("""DROP TABLE IF EXISTS stateRate;""")
 
 c.execute(
     """
-    CREATE TABLE creditCardDetails (
+    CREATE TABLE stateRate (
         id INTEGER PRIMARY KEY,
         jurisdiction TEXT,
         stateRate REAL 
     );
     """
 )
+
+with open("tax_model_backend/database/2020-state-tax-rates.csv","r") as state_rates:
+    reader_file = csv.reader(state_rates)
+    for row in reader_file:
+        c.execute("""INSERT INTO stateRate ("jurisdiction", "stateRate") VALUES (?,?);""", row)
 
 conn.commit()
 c.close()
