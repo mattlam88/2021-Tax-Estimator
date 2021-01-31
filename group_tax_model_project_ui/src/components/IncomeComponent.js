696e81ba36b653585dd477460ee882aa440a3940
David Sage <dkstenor@mac.com>
2021-01-30 22:37:58 -0500

    State Dropdown and Graph placeholders

--- group_tax_model_project_ui/src/components/IncomeComponent.js
+++ group_tax_model_project_ui/src/components/IncomeComponent.js
@@ -1,36 +1,77 @@
 import React, { useState } from 'react';
-import { Container} from 'reactstrap';
+import { Container, Row, Col, Form, FormGroup, Input} from 'reactstrap';
 import Slider from 'react-rangeslider';
 import NumberFormat from 'react-number-format';
 import 'react-rangeslider/lib/index.css';
 
 function IncomeComponent() {
 
-    const [volume, setVolume] = useState(0);
+    const [income, setIncome] = useState(0);
+
+    const [stateTax, setStateTax] = useState('');
     
     const lab = {100000: '$100,000', 200000: '$200,000', 300000: '$300,000',
         400000: '$400,000', 500000: '$500,000', 600000: '$600,000', 700000: '$700,000',
         800000: '$800,000', 900000: '$900,000', 1000000: '$1,000,000'}
     
     function onChange(value) {
-        setVolume(value);
+        setIncome(value);
     }
 
+    function handleChange(e) {
+        let inc_state = e.target;
+        setStateTax(inc_state.value);
+        console.log(inc_state.value);
+        console.log(stateTax);
+    }
     return (
         <React.Fragment>
             <Container>
-                <div style={{textAlign: 'center'}}> 
-                    <NumberFormat value={volume} displayType={'text'} thousandSeparator={true} prefix={'$'} />
-                </div>
-            <Slider
-                    value={volume}
-                    min={0}
-                    max={1000000}
-                    step={10000}
-                    orientation={"horizontal"}
-                    labels={lab}
-                    onChange={onChange}
-                />
+                <Row className="mb-5">
+                    <Col>
+                        <div style={{textAlign: 'center'}}>
+                            Income:&nbsp;&nbsp;
+                            <NumberFormat value={income} displayType={'text'} thousandSeparator={true} prefix={'$'} />
+                        </div>
+                        <Slider
+                                value={income}
+                                min={0}
+                                max={1000000}
+                                step={10000}
+                                orientation={"horizontal"}
+                                tooltip={false}
+                                labels={lab}
+                                onChange={onChange}
+                            />
+                        </Col>
+                </Row>
+               
+                <Row className="mb-5 mt-5">
+                    <Col md={5}></Col>
+                    <Col md={2}>
+                        <div style={{textAlign: 'center'}}>
+                            State of Residence:&nbsp;&nbsp;
+                            <Form onChange={handleChange} value={stateTax}>
+                            <FormGroup>
+                                <Input type="select" name="select">
+                                <option>AZ</option>
+                                <option>AL</option>
+                                <option>AK</option>
+                                </Input>
+                            </FormGroup>
+                            </Form>
+                        </div>
+                    </Col>
+                    <Col md={5}></Col>
+                </Row>
+                <Row>
+                    <Col className="block-example border border-dark p-3 m-3">
+                        Graph 1
+                    </Col>
+                    <Col className="block-example border border-dark p-3 m-3">
+                        Graph 2
+                    </Col>
+                </Row>
             </Container>
         </React.Fragment>
     )
