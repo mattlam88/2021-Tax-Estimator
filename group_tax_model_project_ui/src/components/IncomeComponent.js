df1b5ac37f88c43de36cd07cdbc68f5ba6c420ce
mattlam88 <63694045+mattlam88@users.noreply.github.com>
2021-02-09 00:00:56 -0500

    added axios, included more state options

--- group_tax_model_project_ui/src/components/IncomeComponent.js
+++ group_tax_model_project_ui/src/components/IncomeComponent.js
@@ -1,21 +1,24 @@
 import React, { useState } from 'react';
-import { Container, Row, Col, Form, FormGroup, Input} from 'reactstrap';
+import { Container, Row, Col, Form, FormGroup, Input } from 'reactstrap';
 import Slider from 'react-rangeslider';
 import NumberFormat from 'react-number-format';
 import 'react-rangeslider/lib/index.css';
+import axios from 'axios'
 
 function IncomeComponent() {
 
     const [income, setIncome] = useState(0);
 
     const [stateTax, setStateTax] = useState('');
-    
-    const lab = {100000: '$100,000', 200000: '$200,000', 300000: '$300,000',
+
+    const lab = {
+        100000: '$100,000', 200000: '$200,000', 300000: '$300,000',
         400000: '$400,000', 500000: '$500,000', 600000: '$600,000', 700000: '$700,000',
-        800000: '$800,000', 900000: '$900,000', 1000000: '$1,000,000'}
-    
+        800000: '$800,000', 900000: '$900,000', 1000000: '$1,000,000'
+    }
+
     const states = [
-        AL, AK, AZ, AR, CA, CO, CT, DE, DC, FL, GA, HI, ID, IL, IN, IA, KS, KY, LA, ME, MD, MA, MI, MN, MS, MO, MT, NE, NV, NH, 
+        AL, AK, AZ, AR, CA, CO, CT, DE, DC, FL, GA, HI, ID, IL, IN, IA, KS, KY, LA, ME, MD, MA, MI, MN, MS, MO, MT, NE, NV, NH,
         NJ, NM, NY, NC, ND, OH, OK, OR, PA, RI, SC, SD, TN, TX, UT, VT, VA, WA, WV, WI, WY
     ]
     function onChange(value) {
@@ -33,42 +36,90 @@ function IncomeComponent() {
             <Container>
                 <Row className="mb-5">
                     <Col>
-                        <div style={{textAlign: 'center'}}>
+                        <div style={{ textAlign: 'center' }}>
                             Income:&nbsp;&nbsp;
                             <NumberFormat value={income} displayType={'text'} thousandSeparator={true} prefix={'$'} />
                         </div>
                         <Slider
-                                value={income}
-                                min={0}
-                                max={1000000}
-                                step={10000}
-                                orientation={"horizontal"}
-                                tooltip={false}
-                                labels={lab}
-                                onChange={onChange}
-                            />
-                        </Col>
+                            value={income}
+                            min={0}
+                            max={1000000}
+                            step={10000}
+                            orientation={"horizontal"}
+                            tooltip={false}
+                            labels={lab}
+                            onChange={onChange}
+                        />
+                    </Col>
                 </Row>
-               
+
                 <Row className="mb-5 mt-5">
                     <Col md={5}></Col>
                     <Col md={2}>
-                        <div style={{textAlign: 'center'}}>
+                        <div style={{ textAlign: 'center' }}>
                             State of Residence:&nbsp;&nbsp;
                             <Form onChange={handleChange} value={stateTax}>
-                            <FormGroup>
-                                <Input type="select" name="select">
-                                <option>AZ</option>
-                                <option>AL</option>
-                                <option>AK</option>
-                                </Input>
-                            </FormGroup>
+                                <FormGroup>
+                                    <Input type="select" name="select">
+                                        <option>AZ</option>
+                                        <option>AL</option>
+                                        <option>AK</option>
+                                        <option>AR</option>
+                                        <option>CA</option> 
+                                        <option>CO</option>
+                                        <option>CT</option>
+                                        <option>DE</option>
+                                        <option>DC</option>
+                                        <option>FL</option>
+                                        <option>GA</option>
+                                        <option>HI</option>
+                                        <option>ID</option>
+                                        <option>IL</option>
+                                        <option>IN</option>
+                                        <option>IA</option>
+                                        <option>KS</option>
+                                        <option>KY</option>
+                                        <option>LA</option>
+                                        <option>ME</option>
+                                        <option>MD</option>
+                                        <option>MA</option>
+                                        <option>MI</option>
+                                        <option>MN</option>
+                                        <option>MS</option>
+                                        <option>MO</option>
+                                        <option>MT</option>
+                                        <option>NE</option>
+                                        <option>NV</option>
+                                        <option>NH</option>
+                                        <option>NJ</option>
+                                        <option>NM</option>
+                                        <option>NY</option>
+                                        <option>NC</option>
+                                        <option>ND</option>
+                                        <option>OH</option>
+                                        <option>OK</option>
+                                        <option>OR</option>
+                                        <option>PA</option>
+                                        <option>RI</option>
+                                        <option>SC</option>
+                                        <option>SD</option>
+                                        <option>TN</option>
+                                        <option>TX</option>
+                                        <option>UT</option>
+                                        <option>VT</option>
+                                        <option>VA</option>
+                                        <option>WA</option>
+                                        <option>WV</option>
+                                        <option>WI</option>
+                                        <option>WY</option>
+                                        </Input>
+                                </FormGroup>
                             </Form>
                         </div>
                     </Col>
                     <Col md={5}></Col>
                 </Row>
-                <Row> 
+                <Row>
                     {/* Do we want to make each of these Graphs their own componenent? */}
                     <Col className="block-example border border-dark p-3 m-3">
                         Graph 1
