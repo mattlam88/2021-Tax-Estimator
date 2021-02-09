a516548f25cd145e950883158be572d62ca7db9e
mattlam88 <63694045+mattlam88@users.noreply.github.com>
2021-02-08 22:55:01 -0500

    updated database and created new table

--- tax_model_backend/database/seed.py
+++ tax_model_backend/database/seed.py
@@ -5,6 +5,7 @@ conn = sqlite3.connect("taxModel.db")
 c = conn.cursor()
 
 c.execute("""DROP TABLE IF EXISTS stateRate;""")
+c.execute("""DROP TABLE IF EXISTS trumpBidenRates""")
 
 c.execute(
     """
@@ -20,6 +21,22 @@ with open("tax_model_backend/database/2020-state-tax-rates.csv","r") as state_ra
     reader_file = csv.reader(state_rates)
     for row in reader_file:
         c.execute("""INSERT INTO stateRate ("jurisdiction", "stateAvgRate") VALUES (?,?);""", row)
+
+c.execute(
+    """
+    CREATE TABLE trumpBidenRates (
+        id INTEGER PRIMARY KEY,
+        presidentBracket TEXT,
+        fedTaxRate REAL,
+        incomeBracket TEXT 
+    );
+    """
+)
+
+with open("tax_model_backend/database/Biden_Trump_tax_rates.csv","r") as fed_rates:
+    reader_file = csv.reader(fed_rates)
+    for row in reader_file:
+        c.execute("""INSERT INTO trumpBidenRates ("presidentBracket", "fedTaxRate", "incomeBracket") VALUES (?,?,?);""", row)
 
 conn.commit()
 c.close()
